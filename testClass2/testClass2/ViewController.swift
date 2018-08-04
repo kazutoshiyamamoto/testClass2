1c9a6f7edec37fa10724ca592755269ce90e8192
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-04 23:30:08 +0900

    インスタンス自身のvalueにアクセスするselfキーワード

---
+++ testClass2/testClass2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  testClass2
+//
+//  Created by home on 2018/08/04.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    struct SomeStruct {
+        let value = 123
+        
+        func printValue() {
+            print(self.value)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
