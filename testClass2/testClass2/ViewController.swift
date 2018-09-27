655c9501de2ee6d85198a7dd2623fc9503f69a0e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-09-27 23:59:57 +0900

    変数a,bのうちaの値のみ変更する

--- testClass2/testClass2/ViewController.swift
+++ testClass2/testClass2/ViewController.swift
@@ -16,11 +16,24 @@ class ViewController: UIViewController {
         var blue: Int
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //
+        var a = Color(red: 255, green: 0, blue: 0)
+        var b = a
+        a.red = 0
+        
+        //
+        print(a.red)
+        print(a.green)
+        print(a.blue)
+        
+        //
+        print(b.red)
+        print(b.green)
+        print(b.blue)
     }
     
     override func didReceiveMemoryWarning() {
